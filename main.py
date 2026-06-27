15a9c1caab1342b388ad78b8203ada7bfa4ece8e
Evan Fellman <evan.fellman@gmail.com>
2026-06-27 00:33:41 -0700

    feat: allow for multiple config files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser(description="A fun aquarium experiment")
 parser.add_argument("--highlight-witnessed", action='store_true', help='Any food that is visible by any agent will be highlighted')
 parser.add_argument("--load-prev-nets", action="store_true", help="Load previously pretrained agent models")
 parser.add_argument("--trail", action="store_true", help="Show agents' paths")
+parser.add_argument("--config", default="config.yml", help="Config file path")
 args = parser.parse_args()
 
 import tkinter as tk
@@ -12,7 +13,7 @@ import yaml
 
 
 if __name__ == "__main__":
-    with open("config.yml", "r") as f:
+    with open(args.config, "r") as f:
         config = yaml.safe_load(f)
     window = tk.Tk()
     sim = Simulation(window, config=config, args=args)
